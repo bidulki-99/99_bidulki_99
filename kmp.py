2219f9586f593b4e578dcc4ae39b40dd3754b747
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-06-24 03:10:52 +0900

    Update kmp.py

--- kmp.py
+++ kmp.py
@@ -6,25 +6,23 @@ def failure(p, table):
     j = 0
     for i in range(1, len(p)):
         while j > 0 and p[i] != p[j]:
-            j = table[j-1]
+            j = table[j - 1]
         if p[i] == p[j]:
             j += 1
             table[i] = j
-    return table
-
-F = failure(p, table)
+            
 find = 0
 f = []
 def kmp(i, j):
     global find
     for i in range(len(t)):
         if j > 0 and t[i] != p[j]:
-            j = F[j-1]
+            j = table[j-1]
         if t[i] == p[j]:
-            if j == len(p)-1:
+            if j == len(p) - 1:
                 find += 1
-                f.append(i-j+1)
-                j = F[j]
+                f.append(i - j + 1)
+                j = table[j]
             else:
                 j += 1
 
