6a96383ac34b65b07d470d3b1485bf0b2f79dc21
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-06-27 04:52:36 +0900

    Update z.cpp

--- z.cpp
+++ z.cpp
@@ -1,18 +1,20 @@
-int z[1000001];
+vector<int> Z(string s) {
+	int n = s.size();
+	vector<int> z(n, 0);
+	z[0] = n;
 
-void (string s) {
-  int n = s.size();
-    z[0] = n;
-    for (int i = 1, l = 0, r = 0; i < n; i++) {
-      if (i <= r)
-        z[i] = min(r - i, z[i - l]);
+	for (int i = 1, l = 0, r = 0; i < n; i++) {
+		if (i <= r)
+			z[i] = min(r - i, z[i - l]);
 
-      while (s[i + z[i]] == s[z[i]])
-        z[i]++;
+		while (s[i + z[i]] == s[z[i]])
+			z[i]++;
 
-      if (i > r)
-        l = i;	
+		if (i > r)
+			l = i;	
 
-      r = max(r, i + z[i] - 1);
-    }
+		r = max(r, i + z[i] - 1);
+	}
+
+	return z;
 }
